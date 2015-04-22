59ea27be2664a42b399382e4b832d9f33212efec
elsuizo <martin.noblia@openmailbox.org>
2015-04-21 21:09:47 -0300

    prob complet

--- text_count_letters.jl
+++ text_count_letters.jl
@@ -1,26 +1,12 @@
 
-function count_letters(T)
-
-    counter = 0
-    letters = 0x00:0xff
-    set = unique(T.data)
-    d = Dict()
-    
-    for letter in set
-        counter = 0
-        for i in 1:length(T.data)
-            if(letter == T.data[i] )
-                
-                counter += 1
-                
-                d[letter] = counter
-            end
-        end
-    end
-
-    return d
+function count_letters(T::UTF8String)
+    # Compute the probabilities of letters
+    d = Dict{Char, FloatingPoint}()
+    prob = [length(find(T.data .== letter)) for letter in Set(T)] / (length(T))
+    d = {letter => i for i in prob, letter in Set(T)}
+     
+    return d,prob
 
 end
-
-        
+     
 
