e47a495313b567254c25eca810b65ca04c1158c6
elsuizo <martin.noblia@openmailbox.org>
2016-04-10 23:47:54 -0300

    update

--- sfml_tests.jl
+++ sfml_tests.jl
@@ -1,69 +1,109 @@
-#= -------------------------------------------------------------------------
-# @file sfml_tests.jl
-#
-# @date 11/17/15 18:10:46
-# @author Martin Noblia
-# @email martin.noblia@openmailbox.org
-#
-# @brief
-#
-# @detail
-#
-  Licence:
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at
-# your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-
----------------------------------------------------------------------------=#
 using SFML
-
-# setup the window with some anti aliasing
+#=------------------------------------------------------------------------------
+                            Constants
+------------------------------------------------------------------------------=#
+t = 4*pi/4
+a = 0*pi/8
+td = 0.1 * rand()
+ad = 0.2 * rand()
+g = 9.8
+delta = 1/60
+torque = 0
+#=------------------------------------------------------------------------------
+                            window setup 
+------------------------------------------------------------------------------=#
+# Setup the window with some anti aliasing
 settings = ContextSettings()
-settings.antialiasing_level = 3
-window = RenderWindow(VideoMode(800, 600), "Test", settings, 
-                    window_defaultstyle)
+#settings.antialiasing_level = 3
+window = RenderWindow(VideoMode(800, 600), "Rosette test", settings, window_defaultstyle)
 
-# create a event type
+# Framerate limit
+set_framerate_limit(window, 60)
 event = Event()
+circles = CircleShape[]
 
-# create a rectangle 
-rect = RectangleShape()
-set_size(rect, Vector2f(10, 100))
-set_fillcolor(rect, SFML.green)
-set_position(rect, Vector2f(400, 300))
+# Create the three circles
+#=------------------------------------------------------------------------------
+                        create the shapes
+------------------------------------------------------------------------------=#
+for i = 1:3
+    circle = CircleShape()
+    set_radius(circle, 10)
+    set_fillcolor(circle, SFML.red)
+    set_origin(circle, Vector2f(10, 10))
+    push!(circles, circle)
+end
+set_position(circles[3], Vector2f(400, 300))
 
-direction = 0
-moving = false
+# Create the two rectangles
+rectangles = RectangleShape[]
+for i = 1:2
+    rect = RectangleShape()
+    set_size(rect, Vector2f(10, 100))
+    set_fillcolor(rect, SFML.blue)
+    set_origin(rect, Vector2f(5, 0))
+    push!(rectangles, rect)
+end
+set_position(rectangles[1], Vector2f(400, 300))
+
+#=------------------------------------------------------------------------------
+                        principal loop
+------------------------------------------------------------------------------=#
 while isopen(window)
-    # check for events
+    # Check for events
     while pollevent(window, event)
-        # if close window 
         if get_type(event) == EventType.CLOSED
             close(window)
         end
-        # move right 
+        # Allow the user to control the torque with the left and right arrow keys
         if get_type(event) == EventType.KEY_PRESSED
             if get_key(event).key_code == KeyCode.LEFT
-                moving = true
-                direction = -1
+                torque = -1
+            elseif get_key(event).key_code == KeyCode.RIGHT
+                torque = 1
             end
+        else
+            torque = 0
         end
-        
     end
-    if moving
-        set_position(rect, Vector2f(1*direction,0))
+    cycles = 2000
+    # Simulate the physics
+    # The equation of the Rosette is:
+    #(cos(2*π*f₁*t) + cos(2*π*f₂*t))
+#=------------------------------------------------------------------------------
+                                
+------------------------------------------------------------------------------=#    
+    for i = 1:cycles
+        tdd = (1 / (2 - cos(a)*cos(a))) * (td*td*sin(a)*(1+cos(a)) + (2*td+ad)*ad*sin(a) + g*(cos(a)*sin(t+a)-2*sin(t)) - torque*(1 + cos(a)))
+        add = -tdd - tdd*cos(a) - td*td*sin(a) - g*sin(t+a) + torque
+        td += delta * tdd / cycles
+        ad += delta * add / cycles
+        t += delta * td / cycles
+        a += delta * ad / cycles
     end
-    clear(window, SFML.black)
-    draw(window, rect)
-    display(window)
-    moving = false
-end
+    x1 = 400 + 100 * sin(t)
+    y1 = 300 + 100 * cos(t)
+    x2 = 400 + 100 * (sin(t) + sin(a + t))
+    y2 = 300 + 100 * (cos(t) + cos(a + t))
+    # Set the positions of the circles and rectangles
+    set_position(circles[1], Vector2f(x1, y1))
+    set_position(circles[2], Vector2f(x2, y2))
+    set_rotation(rectangles[1], -180*t/pi )
+    set_rotation(rectangles[2], -180*(t+a)/pi )
+    set_position(rectangles[2], Vector2f(x1, y1))
 
+    # T = td*td + 0.5*(td+ad)^2 + td*(td+ad)*cos(a)
+    # V = -g*(2*cos(t)+cos(t+a))
+    # E = T+V
+    # println(E)
+
+    # Draw everything
+    clear(window, SFML.black)
+    for i = 1:length(rectangles)
+        draw(window, rectangles[i])
+    end
+    for i = 1:length(circles)
+        draw(window, circles[i])
+    end
+    display(window)
+end
