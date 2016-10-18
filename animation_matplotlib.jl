1afc52a6192246e7d963be1d784d3598649d6dc6
elsuizo <martin.noblia@openmailbox.org>
2016-10-18 11:40:58 -0300

    animation with matplotlib

--- animation_matplotlib.jl
+++ animation_matplotlib.jl
@@ -28,30 +28,28 @@ using PyCall
 #@pyimport matplotlib.pyplot as plt
 @pyimport matplotlib.animation as anim 
 
-fig = plt.figure()
+fig = plt[:figure]()
 #fig[:set_dpi](100)
 fig[:set_size_inches](7, 6.5)
-ax = plt.axes(xlim=(0, 10), ylim=(0, 10))
-global patch = plt.Circle((5, -5), 0.75, fc="r")
+ax = plt[ :axes ](xlim=(0, 10), ylim=(0, 10))
+global patch = plt[:Circle]((5, -5), 0.75, fc="r")
 
 function init()
-    global patch
     patch[:center] = (5, 5)
     ax[:add_patch](patch)
-    return (patch, None)
+    return patch,nothing
 end
 step = 1
 function animate(i)
-    global patch
     k = i + 1
     x, y = patch[:center]
     x = 5 + 3 * sin(deg2rad(k * step))
     y = 5 + 3 * cos(deg2rad(k * step))
     patch[:center] = (x, y)
-    return (patch, None)
+    return patch,nothing
 
 end
 
 a = anim.FuncAnimation(fig, animate, init_func=init, frames=360, interval=20)
 
-plt.show()
+plt[:show]()
